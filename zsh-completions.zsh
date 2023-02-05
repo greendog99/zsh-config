fe6f1d1dceb7ee76650d5f638a4412078a02e13d
Mike Fischer <mikefischer99@gmail.com>
2023-02-05 13:56:27 -0500

    Update zsh-completions.zsh

--- zsh-completions.zsh
+++ zsh-completions.zsh
@@ -1,5 +1,6 @@
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
   FPATH=$(brew --prefix)/completions/zsh:$FPATH
 
   autoload -Uz compinit
